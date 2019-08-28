cfa601f7e04e3cfc970c36aff7cc8d1e68edca5b
Hiro <hiroshi_nuts@hotmail.com>
2019-08-27 21:12:57 -0300

    Implementando o Logging

--- MinhaApiCompleta/src/DevIO.Api/V1/Controllers/AuthController.cs
+++ MinhaApiCompleta/src/DevIO.Api/V1/Controllers/AuthController.cs
@@ -4,6 +4,7 @@ using DevIO.Api.ViewModels;
 using DevIO.Business.Intefaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
@@ -24,16 +25,19 @@ namespace DevIO.Api.V1.Controllers
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
         private readonly AppSettings _appSettings;
+        private readonly ILogger _logger;
 
         public AuthController(INotificador notificador,
                               SignInManager<IdentityUser> signInManager,
                               UserManager<IdentityUser> userManager,
                               IOptions<AppSettings> appSettings,
-                              IUser user) : base(notificador, user)
+                              IUser user,
+                              ILogger<AuthController> logger) : base(notificador, user)
         {
             _signInManager = signInManager;
             _userManager = userManager;
             _appSettings = appSettings.Value;
+            _logger = logger;
         }
 
         [HttpPost("nova-conta")]
@@ -72,6 +76,7 @@ namespace DevIO.Api.V1.Controllers
 
             if (result.Succeeded)
             {
+                _logger.LogInformation("Usuario " + logInUser.Email + " logado com sucesso");
                 return CustomResponse(await GerarJwt(logInUser.Email));
             }
 
